8d9b800f1e373cfed6d8918d8e3037165bf94e3c
Juan Rivera <juanriv45@gmail.com>
2018-04-12 01:30:19 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
-def highest_rating
-end
+  def highest_rating
+     Show.order(rating: :desc).first.rating
+  end
 def most_popular_show
 end
 def least_popular_show
