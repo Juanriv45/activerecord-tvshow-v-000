c058d4663e740a4abbc5f54f087236f5bd62961f
Juan Rivera <juanriv45@gmail.com>
2018-04-12 02:06:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,7 @@ class Show < ActiveRecord::Base
   def self.highest_rating
      Show.order(rating: :desc).first.rating
   end
+  def self.highest_rating
+     Show.order(rating: :desc).first.name
+  end
 end
